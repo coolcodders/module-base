bc767c4048de24e2ef2c711b8e2165e61ac9af5e
Raju S <rajus@coolcodders.com>
2022-03-20 13:12:04 +0530

    Base Module for Cool Codders Magento 2 Extensions

---
+++ registration.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Cool Codders
+ *
+ * NOTICE OF LICENSE
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category CoolCodders Extension
+ * @package  CoolCodders_Base
+ * @author   Raju S <rajus@coolcodders.com>
+ * @license  OSL 3.0
+ * @link     http://www.coolcodders.com
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'CoolCodders_Base',
+    __DIR__
+);
